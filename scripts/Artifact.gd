b243aba82b1d44b95ec1fb77a0a420200be6214d
fkeyz <fkeyzuwu@gmail.com>
2022-09-11 12:06:19 +0300

    starting time manager thingy

--- scripts/Artifact.gd
+++ scripts/Artifact.gd
@@ -1,15 +1,25 @@
 extends RigidBody2D
 
-export
+var current_effect: ArtifactEffect
+onready var effects = get_tree().get_nodes_in_group("effects")
 
 var picked := false
 var hitted_enemy := false
 
 var FOLLOW_SPEED := 4.0
-var THROW_SPEED := 6.5
+var THROW_SPEED := 900.0
 
 func _ready() -> void:
 	mode = RigidBody2D.MODE_KINEMATIC
+	TimeManager.effect_timer.connect("timeout", self, "_activate_effect")
+	_set_random_effect()
+
+func _activate_effect():
+	current_effect.activate()
+	_set_random_effect()
+	
+func _set_random_effect():
+	current_effect = effects[randi() % effects.size() - 1]
 
 func _physics_process(delta: float) -> void:
 	if picked or hitted_enemy:
@@ -31,7 +41,7 @@ func _on_artifact_body_entered(body: Node) -> void:
 func throw():
 	var throw_direction = get_global_mouse_position() - global_position
 	set_deferred("mode", RigidBody2D.MODE_CHARACTER)
-	call_deferred("apply_central_impulse", throw_direction * THROW_SPEED)
+	call_deferred("apply_central_impulse", throw_direction.normalized() * THROW_SPEED)
 	(get_node("../Player") as Player)._artifact = null
 	picked = false
 	
---
+++ scripts/TimeManager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+onready var effect_timer: Timer
+
+func _ready() -> void:
+	effect_timer = Timer.new()
+	effect_timer.wait_time = 2
+	effect_timer.one_shot = false
+	add_child(effect_timer)
+	effect_timer.start()
