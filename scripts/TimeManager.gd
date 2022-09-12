5755b80d277c5678b552a69ceb39a2c80e356bd5
fkeyz <fkeyzuwu@gmail.com>
2022-09-12 12:17:32 +0300

    add tilemap timer

--- scripts/TimeManager.gd
+++ scripts/TimeManager.gd
@@ -1,10 +1,25 @@
 extends Node
 
 onready var effect_timer: Timer
+onready var tilemap_timer: Timer
+
+var time_for_event := 4.0 # make this faster over time
 
 func _ready() -> void:
 	effect_timer = Timer.new()
-	effect_timer.wait_time = 5
 	effect_timer.one_shot = false
 	add_child(effect_timer)
-	effect_timer.start()
+	effect_timer.start(time_for_event / 2)
+	effect_timer.wait_time = time_for_event
+	
+	tilemap_timer = Timer.new()
+	tilemap_timer.wait_time = time_for_event
+	tilemap_timer.one_shot = false
+	add_child(tilemap_timer)
+	tilemap_timer.start()
+	
+	effect_timer.connect("timeout", self, "debug", ["effect_timer"])
+	tilemap_timer.connect("timeout", self, "debug", ["tilemap_timer"])
+	
+func debug(timer: String):
+	print(timer + "emited")
