fa8562ea739f81f97d751b8df8d6c300c369bfa2
fkeyz <fkeyzuwu@gmail.com>
2022-09-13 16:39:05 +0300

    yea

--- scenes/NextStageSetup.gd
+++ scenes/NextStageSetup.gd
@@ -1,7 +1,8 @@
-extends Sprite
+extends Node2D
 
 onready var world = get_parent()
 onready var spawn_viewport = $"%Viewport"
+onready var screenshot = $"%Screenshot"
 var stage_spawn_pos = Vector2(0, 180)
 
 func setup():
@@ -14,18 +15,17 @@ func screenshot_next_stage():
 	
 	var stage_image = spawn_viewport.get_texture().get_data()
 	var texture = ImageTexture.new()
-	texture.create_from_image(stage_image)
-	set_texture(texture)
-	tween_screenshot()
+	texture.create_from_image(stage_image, 0)
+	screenshot.set_texture(texture)
 
 func tween_screenshot():
-	global_position = stage_spawn_pos
-	z_index = 2
-	modulate.a = 0
+	screenshot.global_position = stage_spawn_pos
+	screenshot.z_index = 2
+	screenshot.modulate.a = 0
 	
 	var tween = create_tween().set_parallel()
 	tween.set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
 	var tween_time = TimeManager.tilemap_timer.wait_time
 	
-	tween.tween_property(self, "global_position", Vector2(0, 0), tween_time)
-	tween.tween_property(self, "modulate", Color(1, 1, 1, 1), tween_time)
+	tween.tween_property(screenshot, "global_position", Vector2(0, 0), tween_time)
+	tween.tween_property(screenshot, "modulate", Color(1, 1, 1, 1), tween_time)
